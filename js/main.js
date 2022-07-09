e09697648fac9ece1cb53d1b95d2c307c21f055f
s2meeeee <s2m9730@gmail.com>
2022-07-09 18:20:47 +0900

    add-youtubeAPI

--- js/main.js
+++ js/main.js
@@ -57,6 +57,59 @@ new Swiper('.notice-line .swiper', {
 new Swiper('.promotion .swiper', {
     slidesPerView:3, //한번에 보여줄 슬라이드 개수
     spaceBetween: 10, //슬라이드 사이 여백
-    
+    centeredSlides:true, //1번 슬라이드가 가운데로 보이게 만든다.
+    loop:true,
+    autoplay: {
+        delay : 5000
+    },
+    pagination :{
+        el : '.promotion .swiper-pagination', //페이지번호 요소 선택자
+        clickable:true //사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation:{
+        nextEl: '.promotion .swiper-button-next',
+        prevEl: '.promotion .swiper-button-prev',
+    }
+
 });
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+    isHidePromotion = !isHidePromotion
+    if(isHidePromotion){
+        //숨김처리
+        promotionEl.classList.add('hide');
+    }else{
+        //보임처리
+        promotionEl.classList.remove('hide');
+    }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size){
+    // gsap.to(요소, 시간, 옵션);
+    gsap.to(
+        selector, //선택자
+        random(1.5, 2.5), //애니메이션 동작 시간 
+        { //옵션
+            y: size,
+            repeat: -1, //무한반복
+            yoyo: true,
+            ease: Power1.easeInOut,
+            delay:random(0, delay)
+    });
+};
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+//toggle-promtion
 
